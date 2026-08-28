d43fc96882c3da429f4b8350455c2e95a8b0f7dc
anikamahns907 <64509221+anikamahns907@users.noreply.github.com>
2026-08-27 21:56:21 -0400

    refactor: route registration separately and clean up navbar interactions

--- client/src/app/features/auth/login/login.component.ts
+++ client/src/app/features/auth/login/login.component.ts
@@ -1,14 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { SupabaseService } from '../../../core/services/supabase.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
   email = '';
@@ -17,7 +17,7 @@ export class LoginComponent {
 
   constructor(
     private supabaseService: SupabaseService,
-    private router: Router
+    private router: Router,
   ) {}
 
   async login() {
