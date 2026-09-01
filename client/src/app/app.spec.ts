cbfa65f9955a2aad4b660b783329b3831c256728
diwakarsandhu1 <diwakarsandhu1@gmail.com>
2026-09-01 15:50:45 -0400

    fix unit test from Jenkins

--- client/src/app/app.spec.ts
+++ client/src/app/app.spec.ts
@@ -19,6 +19,8 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-app');
+    const h1 = compiled.querySelector('h1');
+    expect(h1).toBeTruthy();
+    expect(h1?.textContent).toContain('Hello, my-app');
   });
 });
